7bd9dfdb6ec3bc9c6c93de65c718587febd0e59d
gustavo L <gustalondon2016@gmail.com>
2021-06-01 23:37:38 -0500

    Adiciono funcion para sensado del voltaje

---
+++ funcion_voltaje.ino
@@ -0,0 +1,58 @@
+
+//////////////////////////////////// VARIABLES SENSOR DE VOLTAJE
+double sensorValue1 = 0;
+double sensorValue2 = 0;
+int crosscount = 0;
+int climb_flag = 0;
+int val[100];
+int max_v = 0;
+double VmaxD = 0;
+double VeffD = 0;
+double Veff = 0;
+float VolRms = 0;
+////////////////////////////////////////////////////////////////////////
+
+float sensor_voltaje(){
+    VolRms = 0;
+  int contador = 0;
+  while(contador <= 5){
+  for ( int i = 0; i < 100; i++ ) {
+    sensorValue1 = analogRead(A0);
+    if (analogRead(A0) > 511) {
+      val[i] = sensorValue1;
+    }
+    else {
+      val[i] = 0;
+    }
+    delay(1);
+  }
+
+  max_v = 0;
+
+  for ( int i = 0; i < 100; i++ )
+  {
+    if ( val[i] > max_v )
+    {
+      max_v = val[i];
+    }
+    val[i] = 0;
+  }
+  if (max_v != 0) {
+
+
+    VmaxD = max_v;
+    VeffD = VmaxD / sqrt(2);
+    Veff = (((VeffD - 420.76) / -90.24) * -210.2) + 210.2;
+  }
+  else {
+    Veff = 0;
+  }
+  
+  VmaxD = 0;
+  contador = contador + 1;
+  VolRms= VolRms + Veff;
+}
+  return VolRms/6;
+    
+ 
+}
